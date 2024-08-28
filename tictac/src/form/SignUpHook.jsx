2c398cb8fe591e08218f0c5921abc89d94659950
tuanminhphan45 <tuanminhphan45@gmail.com>
2024-08-28 20:54:02 +0700

    sử dụng validate bình thường

--- tictac/src/form/SignUpHook.jsx
+++ tictac/src/form/SignUpHook.jsx
@@ -2,31 +2,60 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const SignUpHook = () => {
-    const [register, handleSubmit] = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
     const onSubmit = (values) => {};
+
     return (
         <form
+            onSubmit={handleSubmit(onSubmit)}
             className="p-10 w-full max-w-[500px] mx-auto"
             autoComplete="off"
-            onSubmit={handleSubmit(onSubmit)}
         >
             <div className="flex flex-col gap-2 mb-5">
                 <label htmlFor="firstName">Firstname</label>
                 <input
-                    {...register("fristName")}
-                    type="text"
                     id="firstName"
+                    {...register("firstName", {
+                        required: true,
+                        maxLength: 10,
+                    })}
                     placeholder="Enter your first name"
                     className="p-4 rounded-md border border-gray-100"
                 />
+                {errors?.firstName?.type === "required" && (
+                    <div className="text-red-500 text-sm">
+                        Please fill out this field
+                    </div>
+                )}
+                {errors?.firstName?.type === "maxLength" && (
+                    <div className="text-red-500 text-sm">Must be 10 Words</div>
+                )}
             </div>
             <div className="flex flex-col gap-2 mb-5">
                 <label htmlFor="lastName">Last name</label>
                 <input
-                    type="text"
                     id="lastName"
-                    {...register("lastName")}
-                    placeholder="Enter your Last name"
+                    {...register("lastName", {
+                        required: true,
+                        maxLength: 10,
+                    })}
+                    placeholder="Enter your last name"
+                    className="p-4 rounded-md border border-gray-100"
+                />
+            </div>
+            <div className="flex flex-col gap-2 mb-5">
+                <label htmlFor="lastName">Email address</label>
+                <input
+                    id="email"
+                    {...register("email", {
+                        required: true,
+                        maxLength: 10,
+                    })}
+                    placeholder="Enter your Email address"
                     className="p-4 rounded-md border border-gray-100"
                 />
             </div>
